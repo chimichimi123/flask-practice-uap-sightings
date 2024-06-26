3922bb74d65913cd375be2a3ab07b0dfcc53f44f
Christopher Cantu <chrischimi02@gmail.com>
2024-06-26 12:00:23 -0500

    done

--- server/app.py
+++ server/app.py
@@ -1,19 +1,47 @@
 #!/usr/bin/env python3
 
-from flask import Flask, make_response, request
+from models import db, Sighting
+from flask import Flask, make_response, request, jsonify
 from flask_migrate import Migrate
 
 # import model and db instance
 
 # Initialize Flask app
+app = Flask(__name__)
 
 # configure the app
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 # Initialize Flask-Migrate
-
+migrate = Migrate(app, db)
 # Initialize the db instance
-
+db.init_app(app)
 # Define routes and views
+@app.route("/")
+def home():
+    message = {"message": "The UAPID welcome our new extraterrestrial overlords!"}
+    return make_response(jsonify(message), 200)
+
+@app.route('/sightings')
+def sightings():
+    sightings=[]
+    for sighting in Sighting.query.all():
+        sightings.append(sighting.to_dict())
+    body = {'sightings': sightings}
+    return make_response(body, 200)
+
+@app.route('/sightings/<int:id>')
+def sighting_by_id(id):
+    sightings_id = Sighting.query.filter(Sighting.id == id).first()
+    body = {'sighting ids': sightings_id.to_dict()}
+    return make_response(body, 200)
+
+@app.route('/sightings/location/<string:location>')
+def locations(location):
+    sightings_location = Sighting.query.filter(Sighting.location == location).first()
+    body = {'sighting locations': sightings_location.to_dict()}
+    return make_response(body, 200)
 
 if __name__ == "__main__":
     app.run(port=5555, debug=True)
--- server/seed.py
+++ server/seed.py
@@ -1,9 +1,18 @@
 # import app
+from app import app
 # import model and db instance
-
+from models import db, Sighting
 # Define seeding functions (optional: use Faker to help generate fake data)
+def seed_sightings():
+    sightings = [
+        Sighting(date="2021-01-01", time="12:00", location="New York City", shape_of_craft="Triangle", approximate_size=100, approximate_speed=100, description="A large triangle-shaped craft was seen flying over the city.", reporter="John Doe", reporter_reliable_witness=True),
+        Sighting(date="2021-01-02", time="13:00", location="Los Angeles", shape_of_craft="Circle", approximate_size=50, approximate_speed=50, description="A small circle-shaped craft was seen flying over the city.", reporter="Jane Doe", reporter_reliable_witness=False),
+        Sighting(date="2021-01-03", time="14:00", location="Chicago", shape_of_craft="Square", approximate_size=75, approximate_speed=75, description="A medium square-shaped craft was seen flying over the city.", reporter="John Smith", reporter_reliable_witness=True),
+    ]
+    db.session.bulk_save_objects (sightings)
+    db.session.commit()
 
 if __name__ == "__main__":
     with app.app_context():
-        # Call seeding functions
+        seed_sightings()
         pass
